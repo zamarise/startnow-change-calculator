437ab0f14545c1e062a5a4127c2922b4092ae7fc
Zamarise Montoya <zamarise@gmail.com>
2017-08-10 17:56:29 -0700

    Add initial project files

---
+++ README.md
@@ -0,0 +1,26 @@
+# Change Calculator
+> Calculate change due. Seperated in dollars, quarters, dimes, nickels, and pennies.
+
+This was my second project at Origin Code Academy. This Change Calculator was created using HTML, CSS, Bootstrap, jQuery, and JavaScript.
+
+![](astro-weight-calculator-screenshot.png)
+
+## Approach
+
+I took a mobile first approach with this project (as I try to do with all projects, when feasible!) and thus I wanted the design to be minimal with the focus on the functionality of this app.
+
+## Installation
+
+Download all associated files and load them up in your favorite text editor!
+
+## Development setup
+
+This app relies on jQuery and Javascript for its functionality and HTML, CSS, and Bootstrap for its design. To run the calcuator use a local server such as [nodemon](https://www.npmjs.com/package/nodemon).
+
+## Contact Information
+
+Twitter: [@adriftinthesea](https://twitter.com/adriftinthesea)
+
+Email: z@zamarise.com
+
+GitHub: [https://github.com/zamarise](https://github.com/zamarise/)
---
+++ main.js
@@ -0,0 +1,57 @@
+//Set title and h1 to specified text
+$('title, h1').text('Change Calculator');
+
+//Bind handleClickEvent to button
+var button = document.getElementById('calculate-change');
+button.onclick = handleClickEvent
+
+//Calculates the change due
+function calculateChange(cost, paid) {
+
+  var changeObject = {
+    dollars: 0,
+    quarters: 0,
+    dimes: 0,
+    nickels: 0,
+    pennies: 0
+  };
+
+  var change = paid - cost;
+  
+  var x = change * 100;
+
+  changeObject.dollars = Math.floor(x / 100);
+    x = x % 100;
+  
+  changeObject.quarters = Math.floor(x / 25);
+    x = x % 25;
+  
+  changeObject.dimes = Math.floor(x / 10);
+    x = x % 10;
+
+  changeObject.nickels = Math.floor(x / 5);
+    x = x % 5;
+    x = x + .01;
+    
+  changeObject.pennies = Math.floor(x / 1);
+    x = x % 1;
+
+ return changeObject;
+
+}
+
+//Fires when button is clicked
+function handleClickEvent (e){
+
+    var cost = $('#amount-due').val();
+    var paid = $('#amount-received').val();
+    var result = calculateChange(cost, paid);
+    console.log('result', result);
+   
+    $('#dollars-output').text(result.dollars);
+    $('#quarters-output').text(result.quarters);
+    $('#dimes-output').text(result.dimes);
+    $('#nickels-output').text(result.nickels);
+    $('#pennies-output').text(result.pennies);
+
+}
