a07b5795fa17e8b9e429bb0bc825cfbe889ad431
Zamarise Montoya <zamarise@gmail.com>
2017-09-16 00:20:17 -0700

    [cleanup] cosmetic

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ $('title, h1').text('Change Calculator');
 
 //Bind handleClickEvent to button
 var button = document.getElementById('calculate-change');
-button.onclick = handleClickEvent
+button.onclick = handleClickEvent;
 
 //Calculates the change due
 function calculateChange(cost, paid) {
-
   var changeObject = {
     dollars: 0,
     quarters: 0,
@@ -17,41 +16,38 @@ function calculateChange(cost, paid) {
   };
 
   var change = paid - cost;
-  
+
   var x = change * 100;
 
   changeObject.dollars = Math.floor(x / 100);
-    x = x % 100;
-  
+  x = x % 100;
+
   changeObject.quarters = Math.floor(x / 25);
-    x = x % 25;
-  
+  x = x % 25;
+
   changeObject.dimes = Math.floor(x / 10);
-    x = x % 10;
+  x = x % 10;
 
   changeObject.nickels = Math.floor(x / 5);
-    x = x % 5;
-    x = x + .01;
-    
+  x = x % 5;
+  x = x + 0.01;
+
   changeObject.pennies = Math.floor(x / 1);
-    x = x % 1;
+  x = x % 1;
 
- return changeObject;
-
+  return changeObject;
 }
 
 //Fires when button is clicked
-function handleClickEvent (e){
+function handleClickEvent(e) {
+  var cost = $('#amount-due').val();
+  var paid = $('#amount-received').val();
+  var result = calculateChange(cost, paid);
+  console.log('result', result);
 
-    var cost = $('#amount-due').val();
-    var paid = $('#amount-received').val();
-    var result = calculateChange(cost, paid);
-    console.log('result', result);
-   
-    $('#dollars-output').text(result.dollars);
-    $('#quarters-output').text(result.quarters);
-    $('#dimes-output').text(result.dimes);
-    $('#nickels-output').text(result.nickels);
-    $('#pennies-output').text(result.pennies);
-
+  $('#dollars-output').text(result.dollars);
+  $('#quarters-output').text(result.quarters);
+  $('#dimes-output').text(result.dimes);
+  $('#nickels-output').text(result.nickels);
+  $('#pennies-output').text(result.pennies);
 }
